77faff3af135a576abc5a55e19ad23ed3b2889a4
Talaat Ramadan <talatramadan10@gmail.com>
2026-07-26 02:11:35 +0300

    feat: add admin role authorization to category and product controllers

--- myshop.Web/Controllers/CategoryController.cs
+++ myshop.Web/Controllers/CategoryController.cs
@@ -1,87 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using myshop.Entities.Models;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ShopHub.Business.Dtos.Category;
 using ShopHub.Business.Interfaces.Services;
+using ShopHub.Entities.Constants;
 
-namespace myshop.Web.Areas.Admin.Controllers
+namespace myshop.Web.Areas.Admin.Controllers;
+
+[Authorize(Roles = Roles.Admin)]
+public class CategoryController(ICategoryService categoryService) : Controller
 {
-    public class CategoryController(ICategoryService categoryService) : Controller
+    private readonly ICategoryService _categoryService = categoryService;
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
-        private readonly ICategoryService _categoryService = categoryService;
+        var categories = await _categoryService.GetAllAsync(cancellationToken);
+        return View(categories);
+    }
 
-        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CategoryDto categoryDto, CancellationToken cancellationToken)
+    {
+        if (ModelState.IsValid)
         {
-            var categories = await _categoryService.GetAllAsync(cancellationToken);
-            return View(categories);
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(CategoryDto categoryDto, CancellationToken cancellationToken)
-        {
-            if (ModelState.IsValid)
-            {
-                await _categoryService.CreateAsync(categoryDto, cancellationToken);
-                TempData["Create"] = "Item has Created Successfully";
-                return RedirectToAction("Index");
-            }
-            return View(categoryDto);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Edit(int? id, CancellationToken cancellationToken)
-        {
-            if (id == null | id == 0)
-                return BadRequest();
-            
-            var categoryIndb = await _categoryService.GetByIdAsync(id!.Value, cancellationToken);
-
-            if (categoryIndb == null)
-                return NotFound();
-
-            return View(categoryIndb);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(CategoryDto categoryDto)
-        {
-            if (ModelState.IsValid)
-            {
-                await _categoryService.UpdateAsync(categoryDto);
-
-                TempData["Update"] = "Data has Updated Successfully";
-                return RedirectToAction("Index");
-            }
-            return View(categoryDto);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Delete(int? id, CancellationToken cancellationToken)
-        {
-            if (id == null | id == 0)
-                return BadRequest();
-
-            var categoryIndb = await _categoryService.GetByIdAsync(id!.Value, cancellationToken);
-
-            if (categoryIndb is null)
-                return NotFound();
-            
-            return View(categoryIndb);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> DeleteCategory(int? id, CancellationToken cancellationToken)
-        {
-            
-            await _categoryService.DeleteAsync(id!.Value);
-
-            TempData["Delete"] = "Item has Deleted Successfully";
+            await _categoryService.CreateAsync(categoryDto, cancellationToken);
+            TempData["Create"] = "Item has Created Successfully";
             return RedirectToAction("Index");
         }
+        return View(categoryDto);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id, CancellationToken cancellationToken)
+    {
+        if (id == null | id == 0)
+            return BadRequest();
+        
+        var categoryIndb = await _categoryService.GetByIdAsync(id!.Value, cancellationToken);
+
+        if (categoryIndb == null)
+            return NotFound();
+
+        return View(categoryIndb);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(CategoryDto categoryDto)
+    {
+        if (ModelState.IsValid)
+        {
+            await _categoryService.UpdateAsync(categoryDto);
+
+            TempData["Update"] = "Data has Updated Successfully";
+            return RedirectToAction("Index");
+        }
+        return View(categoryDto);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id, CancellationToken cancellationToken)
+    {
+        if (id == null | id == 0)
+            return BadRequest();
+
+        var categoryIndb = await _categoryService.GetByIdAsync(id!.Value, cancellationToken);
+
+        if (categoryIndb is null)
+            return NotFound();
+        
+        return View(categoryIndb);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteCategory(int? id, CancellationToken cancellationToken)
+    {
+        
+        await _categoryService.DeleteAsync(id!.Value);
+
+        TempData["Delete"] = "Item has Deleted Successfully";
+        return RedirectToAction("Index");
     }
 }
--- myshop.Web/Controllers/ProductController.cs
+++ myshop.Web/Controllers/ProductController.cs
@@ -1,165 +1,167 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using myshop.Entities.ViewModels;
 using ShopHub.Business.Dtos.Product;
 using ShopHub.Business.Interfaces.Services;
+using ShopHub.Entities.Constants;
 
-namespace myshop.Web.Areas.Admin.Controllers
+namespace myshop.Web.Areas.Admin.Controllers;
+
+[Authorize(Roles = Roles.Admin)]
+public class ProductController(
+    IWebHostEnvironment webHostEnvironment,
+    IProductService productService,
+    ICategoryService categoryService) : Controller
 {
-    public class ProductController(
-        IWebHostEnvironment webHostEnvironment,
-        IProductService productService,
-        ICategoryService categoryService) : Controller
+    private readonly IProductService _productService = productService;
+    private readonly ICategoryService _categoryService = categoryService;
+    private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
+
+    public IActionResult Index()
     {
-        private readonly IProductService _productService = productService;
-        private readonly ICategoryService _categoryService = categoryService;
-        private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetData()
-        {
-            var productListDto = await _productService.GetAllWithCategoryAsync();
-
-            return Json(new { data = productListDto });
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Create(CancellationToken cancellationToken)
-        {
-            var categoriesDto = await _categoryService.GetAllAsync(cancellationToken);
-            ProductViewModel productViewModel = new ProductViewModel()
-            {
-                ProductDto = new ProductDto(),
-                CategoryList = categoriesDto.Select(x => new SelectListItem
-                {
-                    Text = x.Name,
-                    Value = x.Id.ToString()
-                })
-            };
-            return View(productViewModel);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(ProductViewModel productViewModel, IFormFile file)
-        {
-            if (ModelState.IsValid)
-            {
-                string RootPath = _webHostEnvironment.WebRootPath;
-                if (file != null)
-                {
-                    string filename = Guid.NewGuid().ToString();
-                    var Upload = Path.Combine(RootPath, @"Images\Products");
-                    var ext = Path.GetExtension(file.FileName);
-
-                    using (var filestream = new FileStream(Path.Combine(Upload,filename+ext),FileMode.Create))
-                    {
-                        file.CopyTo(filestream);
-                    }
-                    productViewModel.ProductDto.Img = @"Images\Products\" + filename + ext;
-                }
-
-                await _productService.CreateAsync(productViewModel.ProductDto);
-
-                TempData["Create"] = "Item has Created Successfully";
-                return RedirectToAction("Index");
-            }
-            return View(productViewModel.ProductDto);
-        }
-        [HttpGet]
-        public async Task<IActionResult> Edit(int? id, CancellationToken cancellationToken)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            var prodcutDto = await _productService.GetByIdAsync(id.Value, cancellationToken);
-            var categories = await _categoryService.GetAllAsync(cancellationToken);
-            ProductViewModel productViewModel = new ProductViewModel()
-            {
-                ProductDto = prodcutDto!,
-                CategoryList = categories.Select(x => new SelectListItem
-                {
-                    Text = x.Name,
-                    Value = x.Id.ToString()
-                })
-            };
-
-            return View(productViewModel);
-        }
-        
-        [HttpPost]
-        public async Task<IActionResult> Edit(ProductViewModel productViewModel, IFormFile? file)
-        {
-            if (ModelState.IsValid)
-            {
-                string RootPath = _webHostEnvironment.WebRootPath;
-
-                if (file != null)
-                {
-                    string filename = Guid.NewGuid().ToString();
-                    var Upload = Path.Combine(RootPath, @"Images\Products");
-                    var ext = Path.GetExtension(file.FileName);
-
-                    if (productViewModel.ProductDto.Img != null)
-                    {
-                        var oldimg = Path.Combine(RootPath, productViewModel.ProductDto.Img.TrimStart('\\'));
-
-                        if (System.IO.File.Exists(oldimg))
-                        {
-                            System.IO.File.Delete(oldimg);
-                        }
-                    }
-
-                    using (var filestream = new FileStream(Path.Combine(Upload, filename + ext), FileMode.Create))
-                    {
-                        file.CopyTo(filestream);
-                    }
-
-                    productViewModel.ProductDto.Img = @"Images\Products\" + filename + ext;
-                }
-
-                await _productService.UpdateAsync(productViewModel.ProductDto);
-
-                TempData["Update"] = "Data has Updated Successfully";
-                return RedirectToAction("Index");
-            }
-
-            return View(productViewModel.ProductDto);
-        }
-        
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int? id, CancellationToken cancellationToken)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            var productIndb = await _productService.GetByIdAsync(id!.Value, cancellationToken);
-
-            if (productIndb == null)
-            {
-                return Json(new { success = false, message = "Error while Deleting" });
-            }
-
-            await _productService.DeleteAsync(productIndb.Id);
-
-            var oldimg = Path.Combine(_webHostEnvironment.WebRootPath, productIndb.Img!.TrimStart('\\'));
-
-            if (System.IO.File.Exists(oldimg))
-            {
-                System.IO.File.Delete(oldimg);
-            }
-
-            return Json(new { success = true, message = "file has been Deleted" });
-        }
-
-
+        return View();
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetData()
+    {
+        var productListDto = await _productService.GetAllWithCategoryAsync();
+
+        return Json(new { data = productListDto });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create(CancellationToken cancellationToken)
+    {
+        var categoriesDto = await _categoryService.GetAllAsync(cancellationToken);
+        ProductViewModel productViewModel = new ProductViewModel()
+        {
+            ProductDto = new ProductDto(),
+            CategoryList = categoriesDto.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            })
+        };
+        return View(productViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ProductViewModel productViewModel, IFormFile file)
+    {
+        if (ModelState.IsValid)
+        {
+            string RootPath = _webHostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string filename = Guid.NewGuid().ToString();
+                var Upload = Path.Combine(RootPath, @"Images\Products");
+                var ext = Path.GetExtension(file.FileName);
+
+                using (var filestream = new FileStream(Path.Combine(Upload,filename+ext),FileMode.Create))
+                {
+                    file.CopyTo(filestream);
+                }
+                productViewModel.ProductDto.Img = @"Images\Products\" + filename + ext;
+            }
+
+            await _productService.CreateAsync(productViewModel.ProductDto);
+
+            TempData["Create"] = "Item has Created Successfully";
+            return RedirectToAction("Index");
+        }
+        return View(productViewModel.ProductDto);
+    }
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id, CancellationToken cancellationToken)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var prodcutDto = await _productService.GetByIdAsync(id.Value, cancellationToken);
+        var categories = await _categoryService.GetAllAsync(cancellationToken);
+        ProductViewModel productViewModel = new ProductViewModel()
+        {
+            ProductDto = prodcutDto!,
+            CategoryList = categories.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            })
+        };
+
+        return View(productViewModel);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Edit(ProductViewModel productViewModel, IFormFile? file)
+    {
+        if (ModelState.IsValid)
+        {
+            string RootPath = _webHostEnvironment.WebRootPath;
+
+            if (file != null)
+            {
+                string filename = Guid.NewGuid().ToString();
+                var Upload = Path.Combine(RootPath, @"Images\Products");
+                var ext = Path.GetExtension(file.FileName);
+
+                if (productViewModel.ProductDto.Img != null)
+                {
+                    var oldimg = Path.Combine(RootPath, productViewModel.ProductDto.Img.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(oldimg))
+                    {
+                        System.IO.File.Delete(oldimg);
+                    }
+                }
+
+                using (var filestream = new FileStream(Path.Combine(Upload, filename + ext), FileMode.Create))
+                {
+                    file.CopyTo(filestream);
+                }
+
+                productViewModel.ProductDto.Img = @"Images\Products\" + filename + ext;
+            }
+
+            await _productService.UpdateAsync(productViewModel.ProductDto);
+
+            TempData["Update"] = "Data has Updated Successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View(productViewModel.ProductDto);
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int? id, CancellationToken cancellationToken)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var productIndb = await _productService.GetByIdAsync(id!.Value, cancellationToken);
+
+        if (productIndb == null)
+        {
+            return Json(new { success = false, message = "Error while Deleting" });
+        }
+
+        await _productService.DeleteAsync(productIndb.Id);
+
+        var oldimg = Path.Combine(_webHostEnvironment.WebRootPath, productIndb.Img!.TrimStart('\\'));
+
+        if (System.IO.File.Exists(oldimg))
+        {
+            System.IO.File.Delete(oldimg);
+        }
+
+        return Json(new { success = true, message = "file has been Deleted" });
+    }
+
+
 }
